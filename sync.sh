792738214f3ab75ab96dc4e191b583671e568cbf
Jeremy Thomas <thomasjm@dogo.andrew.cmu.edu>
2018-04-20 12:25:21 -0400

    Add sync script

---
+++ sync.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+GETH=~/ethereum/go/bin/geth
+GETH_SYNC_MODE=$1
+DATA_DIRECTORY=~/ethereum/$GETH_SYNC_MODE/data
+LOG_DIRECTORY=~/ethereum/$GETH_SYNC_MODE/logs
+
+CACHE_SIZE=512
+LOGGING_VERBOSITY=3
+
+mkdir ~/ethereum/$GETH_SYNC_MODE
+mkdir $DATA_DIRECTORY
+mkdir $LOG_DIRECTORY
+NOW=$(date +"%s")
+
+echo "Starting geth in $GETH_SYNC_MODE @ $DATA_DIRECTORY"
+
+
+nohup $GETH --syncmode="$GETH_SYNC_MODE" --cache=$CACHE_SIZE --verbosity $LOGGING_VERBOSITY --datadir $DATA_DIRECTORY > $LOG_DIRECTORY/geth_$NOW.log 2>&1 &
