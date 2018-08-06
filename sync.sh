1538d74c36d5a9d91f4d564a0808605dc511c2b3
Jeremy Thomas <thomasjm@andrew.cmu.edu>
2018-08-06 07:21:20 -0400

    Update sync to enable APIs

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ GETH_SYNC_MODE=$1
 DATA_DIRECTORY=~/ethereum/$GETH_SYNC_MODE/data
 LOG_DIRECTORY=~/ethereum/$GETH_SYNC_MODE/logs
 
-CACHE_SIZE=512
+CACHE_SIZE=16384
 LOGGING_VERBOSITY=3
 
 mkdir ~/ethereum/$GETH_SYNC_MODE
@@ -16,4 +16,6 @@ NOW=$(date +"%s")
 echo "Starting geth in $GETH_SYNC_MODE @ $DATA_DIRECTORY"
 
 
-nohup $GETH --syncmode="$GETH_SYNC_MODE" --cache=$CACHE_SIZE --verbosity $LOGGING_VERBOSITY --datadir $DATA_DIRECTORY > $LOG_DIRECTORY/geth_$NOW.log 2>&1 &
+nohup $GETH --syncmode="$GETH_SYNC_MODE" --cache=$CACHE_SIZE \
+--verbosity $LOGGING_VERBOSITY --datadir $DATA_DIRECTORY \
+--wsapi admin,db,debug,eth,net,txpool,web3 --rpcapi admin,db,debug,eth,net,txpool,web3 --rpc > $LOG_DIRECTORY/geth_$NOW.log 2>&1 &
